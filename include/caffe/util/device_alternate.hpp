cc375575b1f27fe5050d827aae1fcd77c2e91783
oachrack <omer.achrack@intel.com>
2017-09-13 09:57:39 +0300

    support stub GPU on windows

--- include/caffe/util/device_alternate.hpp
+++ include/caffe/util/device_alternate.hpp
@@ -29,6 +29,9 @@ void classname<Dtype>::funcname##_##gpu(const vector<Blob<Dtype>*>& top, \
     const vector<bool>& propagate_down, \
     const vector<Blob<Dtype>*>& bottom) { NO_GPU; } \
 
+#ifdef _WIN32
+#define STUB_GPU(classname)
+#endif
 #else  // Normal GPU + CPU Caffe.
 
 #include <cublas_v2.h>
