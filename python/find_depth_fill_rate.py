03429124de161f1a87b8154b15b4dca04bf46ffd
oachrack <omer.achrack@intel.com>
2018-01-14 14:35:20 +0200

    add depth fill rate script on GT

--- python/find_depth_fill_rate.py
+++ python/find_depth_fill_rate.py
@@ -6,21 +6,28 @@ import matplotlib.pyplot as plt
 import ipdb
 import argparse
 def find_depth_fill_rate(root):
-    total_fill_rate =0
+    total_fill_rate = 0
     num_of_files = 0
     fill_rates =[]
     for root,dirs,files in os.walk(root):
         for file in files:
-            depth = cv2.imread(os.path.join(root,file),0)
-            if depth is None:
+            if 'depth' not in file:
                 continue
-            fill_rate = np.sum(depth ==0)/float(depth.size)
+            depth = cv2.imread(os.path.join(root,file),-1)
+            gt_path = os.path.join(root,file).replace('depth','color_silhuette')
+            gt = cv2.imread(gt_path,0)
+            if depth is None or gt is None:
+                continue
+            indices = gt > 0
+            relevant_pixels = depth[indices]
+            fill_rate = np.sum(relevant_pixels == 0)/float(relevant_pixels.size)
             fill_rates.append(fill_rate)
             total_fill_rate += fill_rate
             num_of_files += 1
     print "average fill rate {} for {} files".format(total_fill_rate/num_of_files,num_of_files)
     plt.title("histogram of depth fill rates")
-    plt.hist(fill_rates,bins=100)
+    weights = np.ones_like(fill_rates)/len(fill_rates)
+    plt.hist(fill_rates,weights = weights,bins=100)
     plt.show()
 
 
